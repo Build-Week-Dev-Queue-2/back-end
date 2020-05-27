9de7555a06bd8b369881828bbee19206cf72479e
Alexander Besse <alexanderjoeb@hotmail.com>
2020-05-27 12:09:40 -0500

    Updated Slack.js

--- Routes/Slack.js
+++ Routes/Slack.js
@@ -10,6 +10,7 @@ const bc = require('bcryptjs');
 server.post('/devdesk', async (req, res) => {
     const text = req.body.text;
     console.log(req.body);
+    return res.status(200).send(req.body.user_id);
     const slack_id = req.body.user_id;
     // -> /devdesk [connect] [username -p password]
     // -> /devdesk connect -u username -p password
